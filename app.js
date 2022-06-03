d2a67ba7813ac47ecffae753bda11f480a04fcde
Ekaterina <Kata_Yurevna@mail.ru>
2022-06-03 22:22:19 +0700

    readonly

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 "use strict";
-var a = 1;
+// const skill: [number, string] = [1, 'Dev'];
+// skill[0] = 2; we can change
+var skill = [1, 'Dev'];
+var skills = ['Dev', 'DevOps'];
+// const skills: Array<string> = ['Dev', 'DevOps']; - also correct
+// const skills: ReadonlyArray<string> = ['Dev', 'DevOps']; - also correct
--- app.ts
+++ app.ts
@@ -1,1 +1,12 @@
-const a = 1;
+// const skill: [number, string] = [1, 'Dev'];
+// skill[0] = 2; we can change
+
+const skill: readonly [number, string] = [1, 'Dev'];
+
+const skills: readonly string[] = ['Dev', 'DevOps'];
+
+// const skills: Array<string> = ['Dev', 'DevOps']; - also correct
+
+// const skills: ReadonlyArray<string> = ['Dev', 'DevOps']; - also correct
+
+// skill.push('hdfg'); - doesn't work
