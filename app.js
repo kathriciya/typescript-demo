ac819ada956c2997d8cdeb4b1005258f50818e22
Ekaterina <Kata_Yurevna@mail.ru>
2022-06-03 23:20:41 +0700

    enums

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 "use strict";
-var a = 1;
+var StatusCode;
+(function (StatusCode) {
+    StatusCode[StatusCode["SUCCESS"] = 1] = "SUCCESS";
+    StatusCode["IN_PROCESS"] = "p";
+    StatusCode["FAILED"] = "f";
+})(StatusCode || (StatusCode = {}));
+// 1 - успех
+// 'p' - в процессе
+// 'f' - отклонён
+var res = {
+    message: 'Платёж успешен',
+    statusCode: StatusCode.SUCCESS,
+};
+if (res.statusCode === StatusCode.SUCCESS) {
+}
+function action(status) { }
+action(StatusCode.SUCCESS);
+var res2 = 1 /* ADMIN */;
--- app.ts
+++ app.ts
@@ -1,1 +1,27 @@
-const a = 1;
+enum StatusCode {
+  SUCCESS = 1,
+  IN_PROCESS = 'p',
+  FAILED = 'f',
+}
+
+// 1 - успех
+// 'p' - в процессе
+// 'f' - отклонён
+
+const res = {
+  message: 'Платёж успешен',
+  statusCode: StatusCode.SUCCESS,
+};
+
+if (res.statusCode === StatusCode.SUCCESS) {
+}
+function action(status: StatusCode) {}
+
+action(StatusCode.SUCCESS);
+
+const enum Roles {
+  ADMIN = 1,
+  USER = 2,
+}
+
+const res2 = Roles.ADMIN;
