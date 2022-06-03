ef7bfc96e51e23ffafb071c395165b1e2dab8bb4
Ekaterina <Kata_Yurevna@mail.ru>
2022-06-03 12:47:31 +0700

    types in fn

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 "use strict";
-var a = 1;
+function getFullName(firstname, surname) {
+    return "" + firstname + surname;
+}
+var getFullNameArrow = function (firstname, surname) {
+    return "" + firstname + surname;
+};
+function getFullNameCheck(firstname, surname) {
+    if (typeof firstname !== 'string') {
+        throw new Error('!!!');
+    }
+    return "" + firstname + surname;
+}
+console.log(getFullNameCheck(true, false));
--- app.ts
+++ app.ts
@@ -1,1 +1,15 @@
-const a = 1;
+function getFullName(firstname: string, surname: string): string {
+  return `${firstname}${surname}`;
+}
+const getFullNameArrow = (firstname: string, surname: string): string => {
+  return `${firstname}${surname}`;
+};
+
+function сheckFullName(firstname, surname) {
+  if (typeof firstname !== 'string') {
+    throw new Error('!!!');
+  }
+  return `${firstname}${surname}`;
+}
+
+console.log(сheckFullName(true, false));
