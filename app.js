9b1f643016313b256d5cd5c8b59bab2008e077cb
Ekaterina <Kata_Yurevna@mail.ru>
2022-06-03 17:57:27 +0700

    arrays

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 "use strict";
-var a = 1;
+var skills = ['Dev', 'DevOps', 'Testing'];
+// const skills: any[] = ['Dev', 'DevOps', 1]; - bad solution
+for (var _i = 0, skills_1 = skills; _i < skills_1.length; _i++) {
+    var skill = skills_1[_i];
+    console.log(skill.toLowerCase());
+}
+var res = skills
+    .filter(function (s) { return s !== 'DevOps'; })
+    .map(function (s) { return s + '! '; })
+    .reduce(function (a, b) { return a + b; });
+console.log(res);
--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
-const a = 1;
+const skills: string[] = ['Dev', 'DevOps', 'Testing'];
+// const skills: any[] = ['Dev', 'DevOps', 1]; - bad solution
+for (const skill of skills) {
+  console.log(skill.toLowerCase());
+}
+const res = skills
+  .filter((s: string) => s !== 'DevOps')
+  .map((s: string) => s + '! ')
+  .reduce((a, b) => a + b);
+console.log(res);
