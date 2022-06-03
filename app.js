b4d5f338d8626dc578ce7fda92cf244d6892eb76
Ekaterina <Kata_Yurevna@mail.ru>
2022-06-03 21:47:04 +0700

    tuples

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 "use strict";
-var a = 1;
+var skill = [1, 'Dev'];
+// const id = skill[0];
+// const skillName = skill[1];
+// const q = skill[2]; - get error
+// skill.push('hdfhgd');
+// const r = skill[2]; - also get error
+// skill.pop();
+var id = skill[0], skillName = skill[1];
+var arr = [1, 'hgf', true, true, false];
--- app.ts
+++ app.ts
@@ -1,1 +1,11 @@
-const a = 1;
+const skill: [number, string] = [1, 'Dev'];
+// const id = skill[0];
+// const skillName = skill[1];
+// const q = skill[2]; - get error
+
+// skill.push('hdfhgd');
+// const r = skill[2]; - also get error
+// skill.pop();
+const [id, skillName] = skill;
+
+const arr: [number, string, ...boolean[]] = [1, 'hgf', true, true, false];
