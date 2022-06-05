f83bdef8ca45c6d536f830609919441c2c1c98e4
Ekaterina <Kata_Yurevna@mail.ru>
2022-06-05 13:34:44 +0700

    union

--- app.ts
+++ app.ts
@@ -1,1 +1,35 @@
-const a = 1;
+function logId(id: string | number | boolean) {
+  if (typeof id === 'string') {
+    console.log(id);
+  } else if (typeof id === 'number') {
+    console.log(id);
+  } else {
+    console.log(id);
+  }
+}
+
+function logError(err: string | string[]) {
+  if (Array.isArray(err)) {
+    console.log(err);
+  } else {
+    console.log(err);
+  }
+}
+
+function logObject(obj: { a: number } | { b: number }) {
+  if ('a' in obj) {
+    console.log(obj.a);
+  } else {
+    console.log(obj.b);
+  }
+}
+
+function logMultipleIds(a: string | number, b: string | boolean) {
+  if (a === b) {
+  } else {
+    console.log(a);
+  }
+}
+
+// let a: 1 = 1;
+// a = 2;
